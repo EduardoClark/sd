dea54d3f17dcd1d579ecb122aff18a837922b6bf
mvilchis <mvilchis@ciencias.unam.mx>
2017-05-11 19:01:42 -0500

    Add filter by

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ Rails.application.routes.draw do
 
  resources :clinicas
  root 'clinicas#index'
+ post "/" => "clinicas#index"
  get  '/pruebas',    to: 'static_pages#pruebas'
  get  '/informacion',   to: 'static_pages#informacion'
  resources :users
  get  '/encuesta',   to: 'static_pages#encuesta'
  get  '/esentrehombres',   to: 'static_pages#encuesta'
  get  '/centros',   to: 'clinicas#index'
- 
 
 
-    
-    
+
+
+
 end
