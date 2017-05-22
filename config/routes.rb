f6792ea76a20b9e142fbedc13bc67d97ba61c12c
EduardoClark <eclarkgd@gmail.com>
2017-05-22 12:47:51 -0500

    quitar encuesta

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
  get  '/pruebas',    to: 'static_pages#pruebas'
  get  '/informacion',   to: 'static_pages#informacion'
  resources :users
- get  '/encuesta',   to: 'static_pages#encuesta'
- get  '/esentrehombres',   to: 'static_pages#encuesta'
+
  get  '/centros',   to: 'clinicas#index'
     
 end
